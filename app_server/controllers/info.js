3a144e987b64e8ef3b295d5fa4465279c5438cd5
Asantewaa Bremang <freeadwoa@gmail.com>
2019-04-18 19:52:48 +0000

    First node app deploy

--- app_server/controllers/info.js
+++ app_server/controllers/info.js
@@ -7,26 +7,30 @@ var sendJSONresponse = function(res, status, content) {
 module.exports.datainfo = function(req, res, callback){
     //-console.log(req.body)
     var info = new data1();
-    info.accum = req.body.accum
-    if(req.body.accum){
-        //console.log("njbsigr"        
-        info.save()
-        sendJSONresponse(res, 200, {
-            "message": "works"
+    info.accum = req.body.accum    
+    info.save()
+    // //info.accum = req.body.accum
+    // //if(req.body.accum){
+    //     //console.log("njbsigr"    
+    //     info.accum = req.body.accum    
+    //     info.save()
+    //     sendJSONresponse(res, 200, {
+    //         "message": "works"
 
-        });  
+    //     });  
         
-    }
-    else{
-        sendJSONresponse(res, 200, {
-            "message": "does not work"
-        }); 
-        info.save() 
-        sendJSONresponse(res, 200, {
-            "message": "saved"
-        }); 
-        return;
-    }
+    // }
+    // else{
+    //     sendJSONresponse(res, 200, {
+    //         "message": "does not work"
+    //     }); 
+    //     info.accum = req.body.accum
+    //     info.save() 
+    //     sendJSONresponse(res, 200, {
+    //         "message": "saved"
+    //     }); 
+        //return;
+    //}
     
    
 };
